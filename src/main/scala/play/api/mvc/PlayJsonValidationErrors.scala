3ce7562941621c933fd2fe125938b292e6902f5d
Norbert Gogiel <vangogiel@hotmail.co.uk>
2023-06-21 00:14:07 +0100

    Add field is empty validation

--- src/main/scala/play/api/mvc/PlayJsonValidationErrors.scala
+++ src/main/scala/play/api/mvc/PlayJsonValidationErrors.scala
@@ -3,4 +3,5 @@ package play.api.mvc
 object PlayJsonValidationErrors {
   val PathMissing = "error.path.missing"
   val FieldHasInvalidValue = "error.invalidValue"
+  val FieldEmpty = "error.required"
 }
